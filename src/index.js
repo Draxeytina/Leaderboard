2567ea5a5580bde04cef6b61ae3e51b48f452405
Draxeytina <tmurambinda01@gmail.com>
2022-06-16 22:04:12 +0200

    update index.js

--- src/index.js
+++ src/index.js
@@ -1,4 +1,11 @@
 import './style.css';
+
+import postData from './modules/postData.js';
 import renderList from './modules/list.js';
 
-renderList();
+const refreshBtn = document.querySelector('.refresh');
+const submitBtn = document.querySelector('.submit');
+
+window.addEventListener('DOMContentLoaded', renderList);
+refreshBtn.addEventListener('click', renderList);
+submitBtn.addEventListener('click', postData);
